e894a2f40741de3677be3512cf103b78a647aad3
Sreeharsha Paruchuri <venkata.surya@students.iiit.ac.in>
2020-10-24 23:17:29 +0530

    graphs

--- script.py
+++ script.py
@@ -1,9 +1,24 @@
 import os
-os.system("python ./misc/g2o_to_kitti.py ./dataset/gt.g2o ./dataset/gt.kitti")
-os.system("python ./misc/g2o_to_kitti.py ./dataset/edges-poses.g2o ./dataset/edges-poses.kitti")
-os.system("python ./misc/g2o_to_kitti.py ./dataset/sphere.g2o ./dataset/sphere.kitti")
-os.system("python ./misc/g2o_to_kitti.py ./dataset/intel.g2o ./dataset/intel.kitti")
-os.system("python ./misc/g2o_to_kitti.py ./opt_backup.g2o ./dataset/opt.kitti")
-os.system("evo_rpe kitti ./dataset/gt.kitti ./dataset/opt.kitti -v --plot --plot_mode xy")
-os.system("evo_ape kitti ./dataset/gt.kitti ./dataset/opt.kitti -v --plot --plot_mode xy")
-os.system("evo_traj kitti ./dataset/gt.kitti ./dataset/opt.kitti -v --plot --plot_mode xy")
+#os.system("python ./misc/g2o_to_kitti.py ./dataset/opt5_2.g2o ./dataset/opt5_2.kitti")
+#os.system("python ./misc/g2o_to_kitti.py ./dataset/opt1.g2o ./dataset/opt1.kitti")
+#os.system("python ./misc/g2o_to_kitti.py ./dataset/opt2.g2o ./dataset/opt2.kitti")
+#os.system("python ./misc/g2o_to_kitti.py ./dataset/opt3.g2o ./dataset/opt3.kitti")
+#os.system("python ./misc/g2o_to_kitti.py ./dataset/opt4.g2o ./dataset/opt4.kitti")
+#os.system("python ./misc/g2o_to_kitti.py ./dataset/opt5.g2o ./dataset/opt5.kitti")
+os.system("python ./misc/g2o_to_kitti.py ./dataset/opt6.g2o ./dataset/opt6.kitti")
+os.system("python ./misc/g2o_to_kitti.py ./dataset/opt7.g2o ./dataset/opt7.kitti")
+os.system("python ./misc/g2o_to_kitti.py ./dataset/opt8.g2o ./dataset/opt8.kitti")
+os.system("python ./misc/g2o_to_kitti.py ./dataset/opt10.g2o ./dataset/opt10.kitti")
+os.system("python ./misc/g2o_to_kitti.py ./dataset/opt11.g2o ./dataset/opt11.kitti")
+
+#os.system("python ./misc/g2o_to_kitti.py ./dataset/edges-poses.g2o ./dataset/edges-poses.kitti")
+#os.system("python ./misc/g2o_to_kitti.py ./dataset/sphere.g2o ./dataset/sphere.kitti")
+#os.system("python ./misc/g2o_to_kitti.py ./dataset/intel.g2o ./dataset/intel.kitti")
+#os.system("python ./misc/g2o_to_kitti.py ./opt_backup.g2o ./opt_backup.kitti")
+#os.system("evo_rpe kitti ./dataset/gt.kitti ./dataset/opt.kitti -v --plot --plot_mode xy")
+#os.system("evo_ape kitti ./dataset/gt.kitti ./dataset/opt.kitti -v --plot --plot_mode xy")
+#os.system("evo_traj kitti ./dataset/gt.kitti ./dataset/opt.kitti -v --plot --plot_mode xy")
+#evo_rpe kitti ./dataset/opt1.kitti ./dataset/gt.kitti -v --plot --plot_mode xy
+#evo_ape kitti ./dataset/opt1.kitti ./dataset/gt.kitti -v --plot --plot_mode xy
+#evo_traj kitti ./dataset/opt1.kitti ./dataset/gt.kitti -v --plot --plot_mode xy
+#evo_xyz kitti ./dataset/opt1.kitti ./dataset/opt.kitti -v --plot --plot_mode xy
